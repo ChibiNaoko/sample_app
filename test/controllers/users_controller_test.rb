fcab9ec99db68c9fc93918699668e14b4df41879
chibinaoko <tranghuyenttht@gmail.com>
2016-05-02 16:00:18 +0700

    Add user following

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,10 +2,10 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
 
-#   def setup
-#     @user       = users(:michael)
-#     @other_user = users(:archer)
-#   end
+  def setup
+    @user       = users(:michael)
+    @other_user = users(:archer)
+  end
 
  test "should redirect index when not logged in" do
     get :index
@@ -43,4 +43,15 @@ class UsersControllerTest < ActionController::TestCase
 #     assert flash.empty?
 #     assert_redirected_to root_url
 #   end
+
+  test "should redirect following when not logged in" do
+    get :following, id: @user
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get :followers, id: @user
+    assert_redirected_to login_url
+  end
+
 end
